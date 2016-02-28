d6a384e2e40487b0750f6b218cb2d0256a21b4eb
iKachaykin <ivankachaykin707@mail.ru>
2016-02-29 00:42:28 +0200

    Added "try/catch"
    Added an exception handling.

--- LAB1.cs
+++ LAB1.cs
@@ -15,13 +15,24 @@ namespace ConsoleApplication1
             string emailRegularExpression = @"[a-zA-Z0-9]\S*@\w+\.\w*[a-zA-Z]";
             Regex myReg = new Regex(emailRegularExpression);
             Console.WriteLine("Введите полный путь к указанному файлу: ");
-            string directory = Console.ReadLine();
-            string file = File.ReadAllText(@directory);
-            Console.WriteLine("Все адреса электронных почт в указанном файле: ");
-            foreach (Match match in myReg.Matches(file))
+	    try
+	    {
+            	string directory = Console.ReadLine();
+            	string file = File.ReadAllText(@directory);
+            	Console.WriteLine("Все адреса электронных почт в указанном файле: ");
+            	foreach (Match match in myReg.Matches(file))
+            	{
+            		Console.WriteLine(match);
+            	}	
+	    }
+            catch (System.IO.FileNotFoundException)
             {
-            	Console.WriteLine(match);
-            }	
+                Console.WriteLine("Не существует файла или директории с введённым Вами названием!!!");
+            }
+            catch
+            {
+                Console.WriteLine("Непредусмотренная ошибка!!!");
+            }
             Console.ReadKey();
         }
     }
