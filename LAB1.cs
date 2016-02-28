ee350f49de8baf5d1f953c5fd360e22f0374394c
iKachaykin <ivankachaykin707@mail.ru>
2016-02-28 22:24:10 +0200

    Added files via upload
    The project and regular expression created.

---
+++ LAB1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string emailRegularExpression = @"[a-zA-Z0-9]\S*@\w+\.\w*[a-zA-Z]";
+            Regex myReg = new Regex(emailRegularExpression);
+            
+        }
+    }
+}
